dc298bc1fed29bea2af2722178990470253bc083
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-05 16:15:06 +0900

    status bar 사용법

--- App.js
+++ App.js
@@ -6,18 +6,11 @@ import React, { useState } from 'react';
 export default function App() {
   const [number, setNumber] = useState(0);
 
-  const incrementNumber = () => {
-    setNumber(currentNumber => currentNumber + 1);
-  };
-  
-  const decrementNumber = () => {
-    setNumber(currentNumber => currentNumber - 1);
-  };
-
   return (
     <View style={styles.container}>
       <Text style={{ ...styles.text, color: "blue"}}>리액트 네이티브</Text>
       <Text style={styles.text}>리액트 네이티브</Text>
+      {/* <StatusBar backgroundColor="red" barStyle="dark-content" hidden={true}/> */}
       <StatusBar style="auto" />
     </View>
   );
