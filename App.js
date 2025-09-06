2a2d6ba08d982b2ba2a8847a196d4eaa40512072
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-06 15:02:18 +0900

    날씨 APP 레이아웃 구성 및 디자인

--- App.js
+++ App.js
@@ -3,17 +3,79 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 
 
-export default function App() {
+const App = () => {
   const [number, setNumber] = useState(0);
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex:1, backgroundColor: 'red' }}></View>
-      <View style={{ flex:2, height: 100, backgroundColor: 'darkorange' }}></View>
-      <View style={{ flex:2.5, height: 100, backgroundColor: 'green' }}></View>
+    <View style={styles.container}>
+      <View style={styles.cityCon}>
+        <Text style={styles.city}>Busan</Text>
+      </View>
+      <View style={styles.weatherCon}>
+        <View style={styles.day}> 
+          <Text style={styles.regDate}>9월 6일, 토, 14:08</Text>
+          <Text style={styles.desc}>맑음</Text>
+        </View>
+        <View style={styles.tempCon}>
+          <Text style={styles.temp}>24</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFE142",
+  },
+  cityCon: {
+    flex: 1,
+  },
+  city: {
+    flex: 1,
+    marginTop: 50,
+    paddingTop: 20,
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  weatherCon: {
+    flex: 3,
+  },
+  day: {
+    flex: 0.2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  regDate: {
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 15,
+    backgroundColor: "black",
+    color: "white",
+    fontWeight: "bold",
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+  desc: {
+    flex: 1.5,
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  tempCon: {
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  temp: {
+    fontSize: 120,
+  }
+  
+});
+
+export default App;
+
